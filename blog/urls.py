8f31b75e9d93809c379d440842dc4b19380fd76e
borges <joaovitorborgesmatias@Gmail.com>
2018-09-17 10:40:57 -0300

    atualizando delete

--- blog/urls.py
+++ blog/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from publicacoes.views import home, novaPublicacao, editaPublicacao, deletaPublicacao
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='url_home'),
